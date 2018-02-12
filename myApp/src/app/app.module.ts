af5cfe9d7cbff7c42cd86b24d7dbad3a42612bc1
Wojciech Paluch <wojciech.paluch@o2.pl>
2018-02-12 23:12:08 +0100

    added Local Notifcations plugin

--- myApp/src/app/app.module.ts
+++ myApp/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {LocalNotifications} from '@ionic-native/local-notifications';
 
 @NgModule({
   declarations: [
@@ -33,6 +34,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   providers: [
     StatusBar,
+    LocalNotifications,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
--- myApp/src/pages/home/home.ts
+++ myApp/src/pages/home/home.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController, Platform, AlertController} from 'ionic-angular';
+import {LocalNotifications} from '@ionic-native/local-notifications';
+import * as moment from 'moment';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,50 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  notifyTime: any;
+  notifications: any[] = [];
+  days: any[];
+  chosenHours: number;
+  chosenMinutes: number;
 
+  constructor(public navCtrl: NavController, public platform: Platform, public alertCtrl: AlertController, public localNotifications: LocalNotifications) {
+
+    this.platform.ready().then((readySource) => {
+      this.localNotifications.on('click', (notification, state) => {
+        let json = JSON.parse(notification.data);
+
+        let alert = alertCtrl.create({
+          title: notification.title,
+          subTitle: json.mydata
+        });
+        alert.present();
+      })
+    });
+    this.notifyTime = moment(new Date()).format();
+
+    this.chosenHours = new Date().getHours();
+    this.chosenMinutes = new Date().getMinutes();
+
+    this.days = [
+      {title: 'Monday', dayCode: 1, checked: false},
+      {title: 'Tuesday', dayCode: 2, checked: false},
+      {title: 'Wednesday', dayCode: 3, checked: false},
+      {title: 'Thursday', dayCode: 4, checked: false},
+      {title: 'Friday', dayCode: 5, checked: false},
+      {title: 'Saturday', dayCode: 6, checked: false},
+      {title: 'Sunday', dayCode: 0, checked: false}
+    ];
   }
 
+  notification() {
+    this.localNotifications.schedule({
+      title: 'Test',
+      text: 'Text',
+    });
+  }
 }
+
+
+
+
+
